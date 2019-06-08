03745012a3dca9f319b6a9f531725fee07bbc529
pichid <pichid.de@hengleasing.co.th>
2019-06-08 12:45:00 +0700

    Add files

---
+++ ASPNETSerilog.API/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace ASPNETSerilog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            logger.LogDebug("Get values");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            logger.LogDebug("Get by id {0}", id);
+
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ASPNETSerilog.API/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+
+namespace ASPNETSerilog.API
+{
+    public class Program
+    {
+        private const string LogPath = "Logs";
+        private const string SeqURI = "http://localhost:5341";
+        private const string SeqAPIKey = "hmD3OLcgB8sRnLiW5THq";
+
+        public static void Main(string[] args)
+        {
+            string logPath = string.Format(@"{0}\log.txt", LogPath);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File(logPath, rollingInterval: RollingInterval.Hour)
+                .WriteTo.Seq(SeqURI, apiKey: SeqAPIKey)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Start");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Error");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
